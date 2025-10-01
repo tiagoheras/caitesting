19bd1591020c48c203c054afa83d9c36501e83a8
theras <theras@itba.edu.ar>
2025-10-01 10:52:59 -0300

    add safe naming to files before upload

--- src/Components/UploadFilePopup.jsx
+++ src/Components/UploadFilePopup.jsx
@@ -4,6 +4,25 @@ import 'reactjs-popup/dist/index.css';
 import { bucket } from '../utils/supabaseClient';
 import LogIn from './LogIn';
 
+function createSafeFilename(file) {
+    const fileNameParts = file.name.split('.');
+    const fileExtension = fileNameParts.pop().toLowerCase();
+    let baseName = fileNameParts.join('.');
+
+    // Pasa a minúsculas
+    baseName = baseName.toLowerCase();
+    // Reemplaza acentos y caracteres especiales
+    baseName = baseName.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    // Reemplaza espacios y cualquier caracter no alfanumérico por un guión
+    baseName = baseName.replace(/[^a-z0-9]/g, '-').replace(/-+/g, '-');
+
+    const uniquePrefix = Date.now();
+
+    const safeFilename = `${uniquePrefix}-${baseName}.${fileExtension}`;
+    
+    return safeFilename;
+}
+
 function UploadFilePopup({handleLastUpdatedChange, session}) {
     const [open, setOpen] = useState(false);
     const closeModal = () => setOpen(false);
@@ -20,7 +39,7 @@ function UploadFilePopup({handleLastUpdatedChange, session}) {
         console.log(file)
         try {
             const { data, error } = await bucket
-                .upload(`imagenes/${file.name}`, file, {
+                .upload(`imagenes/${createSafeFilename(file)}`, file, {
                     cacheControl: '3600',
                     upsert: false
                 })
